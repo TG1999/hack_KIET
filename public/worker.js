8f0c49395d9a2318b0dddd97344c0002a93d117b
tushar goel <tushar.goel.dav@gmail.com>
2019-02-24 11:43:49 +0530

    notif
    Signed-off-by: tushar goel <tushar.goel.dav@gmail.com>

--- public/worker.js
+++ public/worker.js
@@ -4,7 +4,7 @@ self.addEventListener("push", e => {
   const data = e.data.json();
   console.log("Push Recieved...");
   self.registration.showNotification(data.title, {
-    body: "Notified by Road Relay Give left side to ambulance",
+    body: "Alert!Please make way for Ambulance, ambulance is near you please keep the left most lane empty",
     icon: "http://image.ibb.co/frYOFd/tmlogo.png"
   });
 });
